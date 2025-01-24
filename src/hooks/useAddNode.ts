ab175a58d3a5fb8a0bcd13ab507669fdd865174f
DikshantChauhan <chauhandikshant2734982@gmail.com>
2025-01-24 12:37:28 +0530

    adding node to cetre on canvas

--- src/hooks/useAddNode.ts
+++ src/hooks/useAddNode.ts
@@ -8,24 +8,34 @@ const useNodeChange = () => {
   const setNodeToAdd = useAppStore((state) => state.setNodeToAdd)
   const setSelectedNodeId = useAppStore((state) => state.setSelectedNodeId)
   const editNodeData = useAppStore((state) => state.editNodeData)
-  const {} = useReactFlow()
+  const { getViewport } = useReactFlow()
 
   const handleCloseSideBar = useCallback(() => {
     setSelectedNodeId(null)
     setNodeToAdd(null)
   }, [])
 
-  const changeNode = useCallback((node: AppNode, change: 'add' | 'edit', closeSideBar: boolean = false) => {
-    // const viewPort = getViewport()
-    //calculate current screen center
+  const changeNode = useCallback(
+    (node: AppNode, change: 'add' | 'edit', closeSideBar: boolean = false) => {
+      if (change === 'add') {
+        const viewport = getViewport()
+        const position = {
+          x: (viewport.x * -1 + window.innerWidth / 2 - 150) / viewport.zoom,
+          y: (viewport.y * -1 + window.innerHeight / 2 - 50) / viewport.zoom,
+        }
 
-    if (change === 'add') {
-      setNode({ ...node, dragHandle: '.drag-handle__custom' })
-    } else {
-      editNodeData(node.id, node.data)
-    }
-    closeSideBar && handleCloseSideBar()
-  }, [])
+        setNode({
+          ...node,
+          dragHandle: '.drag-handle__custom',
+          position,
+        })
+      } else {
+        editNodeData(node.id, node.data)
+      }
+      closeSideBar && handleCloseSideBar()
+    },
+    [getViewport]
+  )
 
   return {
     changeNode,
